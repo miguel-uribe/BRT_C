b6d55a6762f566b5841bb3a8096be3cc450b3c27
miguel-uribe <miguel-uribe@users.noreply.github.com>
2023-01-27 16:20:08 -0500

    added the vpython file

---
+++ python/animation.py
@@ -0,0 +1,10 @@
+from vpython import *
+scene.caption = """Right! button drag or Ctrl-drag to rotate "camera" to view scene.
+To zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.
+     On a two-button mouse, middle is left + right.
+Touch screen: pinch/extend to zoom, swipe or two-finger rotate."""
+
+#scene.title = "<script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML'></script>"
+#scene.title += "\\(\\dfrac {5} {7} \\)"
+#print(scene.title)
+
